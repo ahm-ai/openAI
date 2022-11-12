db45f072807b45f294f25efec3020a8b259a8cd7
ahm-ai <user@gmail.com>
2022-11-11 21:20:37 -0600

    Init

--- src/MainComponent/MainComponent.jsx
+++ src/MainComponent/MainComponent.jsx
@@ -19,11 +19,11 @@ export const MainComponent = ({ intent }) => {
                 console.log("Command + Enter");
                 document.querySelector("#submit").click();
             }
-            if (e.key === "c" && e.metaKey) {
-                console.log("Command + C");
-                document.querySelector("#response").focus();
-                document.querySelector("#copy").click();
-            }
+            // if (e.key === "c" && e.metaKey) {
+            //     console.log("Command + C");
+            //     document.querySelector("#response").focus();
+            //     document.querySelector("#copy").click();
+            // }
         })
 
     }, [])
